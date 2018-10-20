f480f48ee9db2255aacc48545a072595dc3c1054
AndreiHr <oneofyouandrey@yahoo.com>
2018-10-20 06:12:23 +0000

    Add account activation

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,7 +22,7 @@ module SessionsHelper
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
+      if user && user.authenticated?(:remember, cookies[:remember_token])
         log_in user
         @current_user = user
       end
@@ -47,4 +47,12 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+  
+  #Redirect the user to the previous page or the default page
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+  
 end
+
